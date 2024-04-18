4ce2988b532936523fbc7d435300d6fc7f4fc5b2
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-18 17:12:04 +0200

    update

---
+++ SwitchUndCase/SwitchUndCase/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SwitchUndCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Alter
+            int age = 16;
+
+            switch (age)
+            {
+                case 15:
+                case 16:
+                case 17:
+                    Console.WriteLine("Zu jung zum Feiern");
+                    break;
+                case 25:
+                    Console.WriteLine("Alles klar, abgehts");
+                    break;
+                // wie Else bei einer If Bedingung
+                default:
+                    Console.WriteLine("Wie alt bist du eigentlich?");
+                    break;
+            }
+
+            if (age == 15)
+            {
+                Console.WriteLine("Zu jung zum Feiern");
+            }
+            else if (age == 25)
+            {
+                Console.WriteLine("Alles klar, abgehts");
+            }
+            else
+            {
+                Console.WriteLine("Wie alt bist du eigentlich?");
+            }
+
+            string username = "I am Groot";
+
+            switch (username)
+            {
+                case "Denis":
+                    Console.WriteLine("Benutzername ist Denis");
+                    break;
+                case "root":
+                    Console.WriteLine("Benutzername ist root");
+                    break;
+                default:
+                    Console.WriteLine("Benutzername unbekannt");
+                    break;
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VerschachtelteIfBedingungen/VerschachtelteIfBedingungen/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VerschachtelteIfBedingungen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAdmin = true;
+            bool isRegistered = true;
+            string userName = ""; // wird hier deklariert userName = Console.ReadLine();
+            Console.WriteLine("Bitte trage deinen Benutzernamen ein!");
+            userName = Console.ReadLine();
+
+            if (isRegistered && userName != "" && userName == "Admin") // UND abfrage ob registriert & nutzername Admin ist, muss beides True sein
+            {
+                Console.WriteLine("Hallo registrierter Nutzer!");
+                Console.WriteLine("Hi {0}", userName);
+                Console.WriteLine("Hallo Admin!");
+
+            }
+
+            if (isAdmin || isRegistered)
+            {
+                Console.WriteLine("Du bist eingeloggt");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
