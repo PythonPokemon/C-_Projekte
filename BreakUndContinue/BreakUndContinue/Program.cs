20fe8032f733ca30122b1b6e6da88669c41a1f56
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-19 12:01:18 +0200

    update

--- BreakUndContinue/BreakUndContinue/Program.cs
+++ BreakUndContinue/BreakUndContinue/Program.cs
@@ -19,8 +19,17 @@ namespace BreakUndContinue
                 Console.WriteLine(counter);
             }
 
-
-
+            // Variante 2 | es wird von null hoch adiert +1 bis kleiner 10
+            for (int counter = 0;counter < 10; counter++)
+            {
+                Console.WriteLine(counter);
+                if (counter  == 3) // aber wenn das ergebnis gleich 3 ist wird abgebrochen
+                {
+                    Console.WriteLine("Bei drei machen wir schluss!");
+                    break;
+                }
+                
+            }
 
             Console.ReadKey();
         }
