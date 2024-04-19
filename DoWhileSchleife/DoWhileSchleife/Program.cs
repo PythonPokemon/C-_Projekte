402a36a4fd0f5f4d706189480163ee9632b54f09
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-19 10:41:30 +0200

    update

---
+++ DoWhileSchleife/DoWhileSchleife/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DoWhileSchleife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthOfText = 0;
+            string wholeText = "";
+            do
+            {
+                Console.WriteLine("Bitte trage den Namen eines Freundes ein");
+                string nameOfAFriend = Console.ReadLine();
+                int currentLength = nameOfAFriend.Length;
+                lengthOfText += currentLength;
+                wholeText += nameOfAFriend;
+            } while (lengthOfText < 20);
+            Console.WriteLine("Danke, das reicht! Deine Freunde sind {0}", wholeText);
+
+            // 1. Zählervariable anlegen
+            int counter = 25;
+            do
+            {
+                Console.WriteLine(counter);
+                // 3. Dafür sorgen, dass Bedingung irgendwann nicht mehr erfüllt ist. 
+                counter++;
+                // 2. Bedingung einfügen
+            } while (counter < 5);
+
+            Console.Read();
+        }
+    }
+}
