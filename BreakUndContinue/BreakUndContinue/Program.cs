0987478472ae9ffccb7c4bfe7131fba3e5aebe65
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-19 11:46:16 +0200

    update

---
+++ BreakUndContinue/BreakUndContinue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BreakUndContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variante 1
+            for (int counter = 0; counter < 10; counter++)
+            {
+
+                if (counter % 2 == 0)
+                {
+                    Console.WriteLine("Als nächstes kommt eine ungerade Zahl");
+                    continue;
+                }
+
+                Console.WriteLine(counter);
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
