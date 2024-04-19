5690ca77a56ae4aa7d2a7f739af5727641f2f4df
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-19 14:56:26 +0200

    update

---
+++ ErsteKlasse/ErsteKlasse/Human.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErsteKlasse
+{
+    // Mensch Klasse
+    // Eine Blaupause für einen Datentypen
+
+    // Füge zwei weitere Membervariablen "eyeColor" und "age" zu Human hinzu.
+    // Erweitere den Konstruktor, sodass er alle vier Variablen verwendet, wenn ein Objekt von Human angelegt wird
+    // Erstelle zwei Objekte von Human
+
+    class Human
+    {
+        // Membervariable Vorname
+        string firstname;
+        // Membervariable Nachname
+        string lastname;
+        string eyeColor;
+        int age;
+
+
+        // Default Konstruktor
+        public Human()
+        {
+            Console.WriteLine("Default Konstruktor aufgerufen");
+            age = 0;
+            eyeColor = "braun";
+            firstname = "Unbekannt";
+            lastname = "Unbekannt";
+        }
+
+        // Parameterisierter Konstruktor
+        public Human(string firstname, string lastname, string eyeColor, int age)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.eyeColor = eyeColor;
+            this.age = age;
+            Console.WriteLine("Objekt von Human erstellt");
+        }
+
+        public Human(string firstname, string lastname, string eyeColor)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.eyeColor = eyeColor;
+        }
+
+        public Human(string firstname, string lastname, int age)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.age = age;
+        }
+
+        public Human(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+
+        // Member Methode
+        public void IntroduceMyself()
+        {
+            if (age != 0 && lastname != null && eyeColor != null && firstname != null)
+            {
+                Console.WriteLine("Hi, Ich bin {0} {1}, meine Augenfarbe ist {2}" +
+                                " und ich bin {3} Jahr alt.", firstname, lastname, eyeColor, age);
+            }
+            else if (lastname != null && eyeColor != null && firstname != null)
+            {
+                Console.WriteLine("Hi, Ich bin {0} {1}, meine Augenfarbe ist {2}" +
+                "", firstname, lastname, eyeColor);
+            }
+            else if (lastname != null && age != 0 && firstname != null)
+            {
+                Console.WriteLine("Hi, Ich bin {0} {1}, und bin {2} Jahre alt" +
+                "", firstname, lastname, age);
+            }
+
+            else if (lastname != null && firstname != null)
+            {
+                Console.WriteLine("Hi, ich bin {0} {1}", firstname, lastname);
+            }
+
+
+        }
+    }
+}
---
+++ ErsteKlasse/ErsteKlasse/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ErsteKlasse
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Erstellen eines Objektes meiner Human Klasse
+            Human denis = new Human("Denis", "Panjuta", "grün", 30);
+            denis.IntroduceMyself();
+
+            Human michael = new Human("Michael", "Müller", "braun", 35);
+            michael.IntroduceMyself();
+
+            Human amalia = new Human("Amalia", "Panjuta", "blau", 1);
+            amalia.IntroduceMyself();
+
+            Human martina = new Human("Martina", "Müller", "grün");
+            martina.IntroduceMyself();
+
+            Human frank = new Human("Frank", "Martinez", 13);
+            frank.IntroduceMyself();
+
+            Human sissy = new Human("Sissy", "DieMissy");
+            sissy.IntroduceMyself();
+
+            Human unbekannterMensch = new Human();
+            unbekannterMensch.IntroduceMyself();
+            Console.ReadKey();
+        }
+    }
+}
