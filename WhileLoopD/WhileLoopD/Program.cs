228f5813ffa1d6b52ef1669242a746d99c563d46
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-19 11:44:49 +0200

    update

--- WhileLoopD/WhileLoopD/Program.cs
+++ WhileLoopD/WhileLoopD/Program.cs
@@ -1,22 +1,24 @@
 ﻿using System;
-
+/*
+ * ist eine kopfgesteuerte schleife
+ */
 namespace WhileLoopD
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int counter = 0;
-            string enteredText = "";
-            // überprüfen ob enteredText ein leerer String ist
-            while (enteredText.Equals(""))
+            int counter = 0; // deklaration | ganzzahl, bezeichner = variable 0
+            string enteredText = ""; // überprüfen ob enteredText ein leerer String ist
+            
+            while (enteredText.Equals(""))// hier wird überprüft , ob der text der eingegeben wurde einen leeren text entspricht
             {
                 Console.WriteLine("Bitte drücke Enter um, um eins hochzuzählen, oder gib etwas anderes ein, um die Zählung zu beenden");
-                // Benutzereingabe
-                enteredText = Console.ReadLine();
+                
+                enteredText = Console.ReadLine();// Benutzereingabe
                 Console.WriteLine("Aktuelle Anzahl der Schüler im Bus: {0}", counter);
-                // Zählen hoch
-                counter++;
+                
+                counter++;// Zählen hoch
             }
             Console.WriteLine("{0} Schüler sind im Bus. Es kann losgehen. Drücke Enter um loszufahren", counter);
 
