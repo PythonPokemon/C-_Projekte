e79af10674e4e6661a1c637b67b22c93ea5dc902
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-19 11:16:33 +0200

    update

---
+++ WhileLoopD/WhileLoopD/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WhileLoopD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            string enteredText = "";
+            // überprüfen ob enteredText ein leerer String ist
+            while (enteredText.Equals(""))
+            {
+                Console.WriteLine("Bitte drücke Enter um, um eins hochzuzählen, oder gib etwas anderes ein, um die Zählung zu beenden");
+                // Benutzereingabe
+                enteredText = Console.ReadLine();
+                Console.WriteLine("Aktuelle Anzahl der Schüler im Bus: {0}", counter);
+                // Zählen hoch
+                counter++;
+            }
+            Console.WriteLine("{0} Schüler sind im Bus. Es kann losgehen. Drücke Enter um loszufahren", counter);
+
+
+            Console.ReadKey();
+        }
+    }
+}
