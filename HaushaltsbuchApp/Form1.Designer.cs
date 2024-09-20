849b9fb7ce5bbcbd8ba47ed719a4ae4e4f12bcac
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-09-20 09:43:54 +0200

    update

---
+++ HaushaltsbuchApp/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace HaushaltsbuchApp
+{
+    partial class Form1
+    {
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.TextBox amountField;
+        private System.Windows.Forms.TextBox descriptionField;
+        private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.ComboBox typeComboBox;
+        private System.Windows.Forms.Label totalIncomeLabel;
+        private System.Windows.Forms.Label totalExpensesLabel;
+        private System.Windows.Forms.Label netBalanceLabel;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button loadButton;
+
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table = new System.Windows.Forms.DataGridView();
+            this.amountField = new System.Windows.Forms.TextBox();
+            this.descriptionField = new System.Windows.Forms.TextBox();
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.typeComboBox = new System.Windows.Forms.ComboBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.totalIncomeLabel = new System.Windows.Forms.Label();
+            this.totalExpensesLabel = new System.Windows.Forms.Label();
+            this.netBalanceLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.SuspendLayout();
+
+            // 
+            // table
+            // 
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Location = new System.Drawing.Point(12, 100);
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(760, 200);
+            this.table.TabIndex = 0;
+            this.table.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+
+            // 
+            // amountField
+            // 
+            this.amountField.Location = new System.Drawing.Point(12, 20);
+            this.amountField.Name = "amountField";
+            this.amountField.Size = new System.Drawing.Size(100, 22);
+            this.amountField.TabIndex = 1;
+
+            // 
+            // descriptionField
+            // 
+            this.descriptionField.Location = new System.Drawing.Point(130, 20);
+            this.descriptionField.Name = "descriptionField";
+            this.descriptionField.Size = new System.Drawing.Size(100, 22);
+            this.descriptionField.TabIndex = 2;
+
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Items.AddRange(new object[] { "Miete", "Lebensmittel", "Unterhaltung", "Transport", "Sonstiges" });
+            this.categoryComboBox.Location = new System.Drawing.Point(250, 20);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(121, 24);
+            this.categoryComboBox.TabIndex = 3;
+
+            // 
+            // typeComboBox
+            // 
+            this.typeComboBox.FormattingEnabled = true;
+            this.typeComboBox.Items.AddRange(new object[] { "Einnahmen", "Ausgaben" });
+            this.typeComboBox.Location = new System.Drawing.Point(390, 20);
+            this.typeComboBox.Name = "typeComboBox";
+            this.typeComboBox.Size = new System.Drawing.Size(121, 24);
+            this.typeComboBox.TabIndex = 4;
+
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(530, 20);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 5;
+            this.addButton.Text = "Hinzufügen";
+            this.addButton.UseVisualStyleBackColor = true;
+
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(12, 320);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Speichern";
+            this.saveButton.UseVisualStyleBackColor = true;
+
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(100, 320);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 7;
+            this.loadButton.Text = "Laden";
+            this.loadButton.UseVisualStyleBackColor = true;
+
+            // 
+            // totalIncomeLabel
+            // 
+            this.totalIncomeLabel.AutoSize = true;
+            this.totalIncomeLabel.Location = new System.Drawing.Point(12, 360);
+            this.totalIncomeLabel.Name = "totalIncomeLabel";
+            this.totalIncomeLabel.Size = new System.Drawing.Size(0, 17);
+            this.totalIncomeLabel.TabIndex = 8;
+
+            // 
+            // totalExpensesLabel
+            // 
+            this.totalExpensesLabel.AutoSize = true;
+            this.totalExpensesLabel.Location = new System.Drawing.Point(12, 380);
+            this.totalExpensesLabel.Name = "totalExpensesLabel";
+            this.totalExpensesLabel.Size = new System.Drawing.Size(0, 17);
+            this.totalExpensesLabel.TabIndex = 9;
+
+            // 
+            // netBalanceLabel
+            // 
+            this.netBalanceLabel.AutoSize = true;
+            this.netBalanceLabel.Location = new System.Drawing.Point(12, 400);
+            this.netBalanceLabel.Name = "netBalanceLabel";
+            this.netBalanceLabel.Size = new System.Drawing.Size(0, 17);
+            this.netBalanceLabel.TabIndex = 10;
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.netBalanceLabel);
+            this.Controls.Add(this.totalExpensesLabel);
+            this.Controls.Add(this.totalIncomeLabel);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.typeComboBox);
+            this.Controls.Add(this.categoryComboBox);
+            this.Controls.Add(this.descriptionField);
+            this.Controls.Add(this.amountField);
+            this.Controls.Add(this.table);
+            this.Name = "Form1";
+            this.Text = "Haushaltsbuch";
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ HaushaltsbuchApp/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HaushaltsbuchApp
+{
+    public partial class Form1 : Form
+    {
+        private DataGridView table;
+        private TextBox amountField;
+        private TextBox descriptionField;
+        private ComboBox categoryComboBox;
+        private ComboBox typeComboBox;
+        private Label totalIncomeLabel;
+        private Label totalExpensesLabel;
+        private Label netBalanceLabel;
+        private double totalIncome = 0;
+        private double totalExpenses = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Tabelle konfigurieren
+            var columns = new string[] { "Betrag", "Beschreibung", "Kategorie", "Typ" };
+            table = new DataGridView
+            {
+                ColumnCount = columns.Length,
+                AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            };
+            for (int i = 0; i < columns.Length; i++)
+            {
+                table.Columns[i].Name = columns[i];
+            }
+
+            // Eingabefelder
+            amountField = new TextBox { Width = 80 };
+            descriptionField = new TextBox { Width = 100 };
+            categoryComboBox = new ComboBox
+            {
+                Items = { "Miete", "Lebensmittel", "Unterhaltung", "Transport", "Sonstiges" },
+                Width = 100
+            };
+            categoryComboBox.SelectedIndex = 0;
+            typeComboBox = new ComboBox
+            {
+                Items = { "Einnahmen", "Ausgaben" },
+                Width = 100
+            };
+            typeComboBox.SelectedIndex = 0;
+
+            // Hinzufügen-Button
+            var addButton = new Button { Text = "Hinzufügen" };
+            addButton.Click += (sender, args) => AddEntry();
+
+            // Layout für Eingaben und Button
+            var inputPanel = new FlowLayoutPanel { Dock = DockStyle.Top };
+            inputPanel.Controls.Add(new Label { Text = "Betrag:" });
+            inputPanel.Controls.Add(amountField);
+            inputPanel.Controls.Add(new Label { Text = "Beschreibung:" });
+            inputPanel.Controls.Add(descriptionField);
+            inputPanel.Controls.Add(new Label { Text = "Kategorie:" });
+            inputPanel.Controls.Add(categoryComboBox);
+            inputPanel.Controls.Add(new Label { Text = "Typ:" });
+            inputPanel.Controls.Add(typeComboBox);
+            inputPanel.Controls.Add(addButton);
+
+            // Gesamtübersicht
+            var summaryPanel = new FlowLayoutPanel { Dock = DockStyle.Bottom };
+            totalIncomeLabel = new Label { Text = "Gesamteinnahmen: 0.00 EUR" };
+            totalExpensesLabel = new Label { Text = "Gesamtausgaben: 0.00 EUR" };
+            netBalanceLabel = new Label { Text = "Restwert: 0.00 EUR" };
+            summaryPanel.Controls.Add(totalIncomeLabel);
+            summaryPanel.Controls.Add(totalExpensesLabel);
+            summaryPanel.Controls.Add(netBalanceLabel);
+
+            // Daten speichern und laden
+            var filePanel = new FlowLayoutPanel { Dock = DockStyle.Left };
+            var saveButton = new Button { Text = "Speichern" };
+            saveButton.Click += (sender, args) => SaveData();
+            var loadButton = new Button { Text = "Laden" };
+            loadButton.Click += (sender, args) => LoadData();
+            filePanel.Controls.Add(saveButton);
+            filePanel.Controls.Add(loadButton);
+
+            // Frame Layout
+            var mainPanel = new Panel { Dock = DockStyle.Fill };
+            mainPanel.Controls.Add(table);
+            this.Controls.Add(mainPanel);
+            this.Controls.Add(inputPanel);
+            this.Controls.Add(summaryPanel);
+            this.Controls.Add(filePanel);
+        }
+
+        // Methode zum Hinzufügen eines Postens
+        private void AddEntry()
+        {
+            if (double.TryParse(amountField.Text, out double amount))
+            {
+                var description = descriptionField.Text;
+                var category = categoryComboBox.SelectedItem.ToString();
+                var type = typeComboBox.SelectedItem.ToString();
+
+                table.Rows.Add(amount.ToString("F2"), description, category, type);
+
+                // Einnahmen und Ausgaben aktualisieren
+                if (type == "Einnahmen")
+                {
+                    totalIncome += amount;
+                }
+                else
+                {
+                    totalExpenses += amount;
+                }
+
+                UpdateSummary();
+                amountField.Clear();
+                descriptionField.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Bitte einen gültigen Betrag eingeben.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Methode zur Aktualisierung der Gesamtübersicht
+        private void UpdateSummary()
+        {
+            totalIncomeLabel.Text = $"Gesamteinnahmen: {totalIncome:F2} EUR";
+            totalExpensesLabel.Text = $"Gesamtausgaben: {totalExpenses:F2} EUR";
+            netBalanceLabel.Text = $"Restwert: {totalIncome - totalExpenses:F2} EUR";
+        }
+
+        // Daten speichern
+        private void SaveData()
+        {
+            using (var writer = new StreamWriter("budget_data.csv"))
+            {
+                foreach (DataGridViewRow row in table.Rows)
+                {
+                    if (!row.IsNewRow)
+                    {
+                        var values = row.Cells.Cast<DataGridViewCell>().Select(cell => cell.Value?.ToString() ?? "");
+                        writer.WriteLine(string.Join(";", values));
+                    }
+                }
+            }
+            MessageBox.Show("Daten erfolgreich gespeichert.", "Erfolg", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Daten laden
+        private void LoadData()
+        {
+            if (File.Exists("budget_data.csv"))
+            {
+                using (var reader = new StreamReader("budget_data.csv"))
+                {
+                    table.Rows.Clear();
+                    totalIncome = 0;
+                    totalExpenses = 0;
+
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var values = line.Split(';');  // Verwende das Semikolon als Trennzeichen
+                        table.Rows.Add(values);
+
+                        double amount = double.Parse(values[0]);
+                        string type = values[3];
+
+                        if (type == "Einnahmen")
+                        {
+                            totalIncome += amount;
+                        }
+                        else if (type == "Ausgaben")
+                        {
+                            totalExpenses += amount;
+                        }
+                    }
+                    UpdateSummary();
+                }
+                MessageBox.Show("Daten erfolgreich geladen.", "Erfolg", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Keine gespeicherten Daten gefunden.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+
+    }
+}
